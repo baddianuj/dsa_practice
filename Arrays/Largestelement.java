b9e52870705ea6f7b3ed5ca636f7c9235825fa00
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-02 15:23:06 +0530

    largestelement

--- Arrays/Largestelement.java
+++ Arrays/Largestelement.java
@@ -1,1 +1,18 @@
-Class Solution
+class Solution {
+    public int largestElement(int[] nums) {
+    int largest = 0;
+    for(int i=0; i<nums.length-1;i++){
+        if(nums[i]>largest) largest = nums[i];
+    }
+    return largest;
+    }
+}
+
+class Main{
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        int arr[] = {2,5,1,3,0};
+        int ans = sol.largestElement(arr);
+        System.out.println(ans);
+    }
+}
