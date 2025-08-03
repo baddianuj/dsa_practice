31be0a591fa37b77bd6bce59a3a43fef3d8fb288
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-03 16:59:53 +0530

    most consective ones in array

---
+++ Arrays/MostCons1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MostCons1 {
+    public static int mostconsective1s(int nums[]){
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        int count=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]==1) {
+                count++;
+            }
+            else{
+                arr.add(count);
+                count=0;
+            }
+        }
+        arr.add(count);
+        return Collections.max(arr);
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,1,1,1,0,1,0,0};
+        int ans = mostconsective1s(arr);
+        System.out.println(ans);
+    }
+
+}
