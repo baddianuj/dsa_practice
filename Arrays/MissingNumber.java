9168ec683007bc89b73ba1e42e05b2b2a2b03a11
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-04 17:56:50 +0530

    brute + better + optimal(sum) missing bnumber

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -17,26 +17,36 @@ public class MissingNumber {
         // return -1;
 
                 //brute
-            int i;
-            for (i=0; i<=nums.length; i++){
-                int flag = 0;
-                for(int j=0; j<nums.length; j++){
-                    if(nums[j]==i){
-                        flag = 1;
-                        break;
-                    }
-                }
-                if (flag==0) break;
+            // int i;
+            // for (i=0; i<=nums.length; i++){
+            //     int flag = 0;
+            //     for(int j=0; j<nums.length; j++){
+            //         if(nums[j]==i){
+            //             flag = 1;
+            //             break;
+            //         }
+            //     }
+            //     if (flag==0) break;
 
-            }
-            return i;
+            // }
+            // return i;
+
+
+                // optimal 1 = sum method
+        int len = nums.length;
+        int sum = (len * (len + 1) )/ 2;
+        int total = 0;
+        for(int i=0; i<len; i++){
+            total+=nums[i];
+        }
+        return sum-total;
 
     }
 
 
 
     public static void main(String[] args) {
-        int  arr[] = {1,2,3,4,5,6,8,9};
+        int  arr[] = {0,1,2,3,4,5,6,8,9};
         int ans = missingNumber(arr);;
         System.out.println(ans);
     }
