9a09eef8d2ab801314b38c94135d58d3342b3e47
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-04 17:42:20 +0530

    brute + better missing bnumber

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -1,16 +1,40 @@
 import java.util.HashSet;
 
 public class MissingNumber {
+
+
+ 
+
     public static int missingNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet<Integer>();
-        for(int i : nums){
-            set.add(i);
-        }
-        for(int i=0; i<=nums.length; i++){
-            if(!set.contains(i)) return i;
-        }
-        return -1;
+               // better solution
+        // HashSet<Integer> set = new HashSet<Integer>();
+        // for(int i : nums){
+        //     set.add(i);
+        // }
+        // for(int i=0; i<=nums.length; i++){
+        //     if(!set.contains(i)) return i;
+        // }
+        // return -1;
+
+                //brute
+            int i;
+            for (i=0; i<=nums.length; i++){
+                int flag = 0;
+                for(int j=0; j<nums.length; j++){
+                    if(nums[j]==i){
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag==0) break;
+
+            }
+            return i;
+
     }
+
+
+
     public static void main(String[] args) {
         int  arr[] = {1,2,3,4,5,6,8,9};
         int ans = missingNumber(arr);;
