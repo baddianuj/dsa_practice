5ed3e8f872e0bd7f051a947af0cec175576f93c5
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-05 21:22:50 +0530

    brute missing no, idk hashing

--- Arrays/SingleNumber.java
+++ Arrays/SingleNumber.java
@@ -1,3 +1,5 @@
+import java.util.*;;
+
 public class SingleNumber {
     public static int singleNumber(int[] nums) {
 
@@ -7,9 +9,9 @@ public class SingleNumber {
 
         // for(int i=0; i<nums.length; i++){
         //     int count=1;
-        //     int num = nums[i];
-        //     for(int j=0; j<nums.length; j++){
-        //         if(nums[j]==num && i!=j){count++;}
+        //     int num = nums[i]; 
+        //     for(int j=0; j<nums.length; j++){ 
+        //         if(nums[j]==num && i!=j){count++;} // or just nums[j]==nums[i]
         //     }
         //     if (count==1) {
         //         return num;
@@ -17,6 +19,21 @@ public class SingleNumber {
 
         // }
         // return -1;
+
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
+        }
+        int i;
+        for( i=0; i<nums.length; i++){
+            if(set.contains(i)) break;
+        }
+        return i;
+
+
+
+
+
     }
     public static void main(String[] args) {
         int arr[] = {1,1,2,3,3,4};
