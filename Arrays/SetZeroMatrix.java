3796c5d3ed24abcff21ccc550aa19ed3c5b560d4
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-07 20:57:10 +0530

    set zero matrix most optimal solution

--- Arrays/SetZeroMatrix.java
+++ Arrays/SetZeroMatrix.java
@@ -5,6 +5,55 @@ public class SetZeroMatrix {
         int rowlen = matrix.length;
         int collen = matrix[0].length;
 
+        boolean firstrow = false;
+        boolean firstcol = false;
+
+        for(int i=0; i<rowlen;i++){
+            if(matrix[i][0]==0){
+                firstrow=true;
+                break;
+            }
+        }
+        for(int j=0; j<collen;j++){
+            if(matrix[0][j]==0){
+                firstcol=true;
+                break;
+            }
+        }
+
+
+        for(int i=1; i<rowlen; i++){
+            for(int j=1; j<collen; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1;i<rowlen;i++){
+            for(int j=1; j<collen; j++){
+                if (matrix[i][0]==0 || matrix[0][j]==0) {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+
+
+
+        if(firstrow){
+            for(int i=0;i<rowlen;i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(firstcol){
+            for(int i=0; i<collen;i++){
+                matrix[0][i]=0;
+            }
+        }
+
+
         //first pass
         // for(int i=0; i<rowlen; i++){
         //     for(int j=0;j<collen; j++){
