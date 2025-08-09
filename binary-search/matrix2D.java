4fdc7f44f1b6b767fa9355ca637426f1335f6198
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-09 17:30:16 +0530

    cvl

--- binary-search/matrix2D.java
+++ binary-search/matrix2D.java
@@ -1,10 +1,4 @@
-class matrix2D{
-    public static boolean search(int matrix[][], int target){
-        return false;
-    }
-    public static void main(String[] args) {
-        int arr[][] ={{}};
-        int target = 0;
-        System.out.println(search(arr, target));
-    }
+public class matrix2D {
+
+    
 }
