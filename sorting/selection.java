28df9f3da2fd7d9ee3d667f2c42992c695808442
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-10 08:12:33 +0530

    Insertion sort - partial understanding

--- sorting/selection.java
+++ sorting/selection.java
@@ -2,21 +2,24 @@ import java.util.*;
 public class selection {
     public static void main(String[] args) {
         int arr[] = {1,3,5,8,4,9,6};
-
-        int i,j,temp,minidx;
-        for(i=0;i<arr.length-1;i++){
-            minidx=i;
-
-            for(j=i+1;j<arr.length;j++){
-                if(arr[j]<arr[minidx]){
-                    minidx=j;
-                }
-            }
-
-            temp = arr[i];
-            arr[i] = arr[minidx];
-            arr[minidx] = temp;
-        }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(selectionsort(arr)));
     }
+    public static int[] selectionsort(int[] arr){
+        int len=arr.length;
+        int min;
+        for(int i=0; i<len-1;i++){
+            min=i;
+            for(int j=i+1;j<len;j++){
+                if(arr[j]<arr[min]) min=j;
+            }
+            swap(arr, min, i);
+        }
+        return arr;
+    }
+    public static void swap(int[] arr, int start, int end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end] = temp;
+    }
+
 }
