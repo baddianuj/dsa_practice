804a6ffc7da4d9cd80fbb0061002ce15dc9d2f67
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-11 11:33:42 +0530

    sum, factorial

--- striversA2Z/recursion/Naturalnos.java
+++ striversA2Z/recursion/Naturalnos.java
@@ -1,8 +1,10 @@
 public class Naturalnos {
     public static void main(String[] args) {
-        int n = 100;
+        int n = 5;
         // print1(n);
-        print2(n,1);
+        // print2(n,1);
+        // System.out.println((sum(n)));
+        System.out.println(factorial(n));
     }
     public static void print1(int n){
         if(n>0){
@@ -16,5 +18,16 @@ public class Naturalnos {
         }
         System.out.println(count);
         print2(n, count+1);
+    }
+    public static int sum(int n){
+        if(n==0) return 0;
+        return n+sum(n-1);
+    }
+    public static int factorial(int n){
+        if(n==1 || n==0){
+            return 1;
+        }
+        return n * factorial(n-1);
+
     }
 }
