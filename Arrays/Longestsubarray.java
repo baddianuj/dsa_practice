3251d6f0afd678a4453efc9951d41c71db0b00d0
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-11 22:21:22 +0530

    longest subarry with hasmap 50 steps 50-35=15 ie target

--- Arrays/Longestsubarray.java
+++ Arrays/Longestsubarray.java
@@ -6,21 +6,37 @@ public class Longestsubarray {
         int target = 15;
         System.out.println(longest(arr, target));
     }
-    public static int longest(int[] arr, int target){
-        ArrayList<Integer> list = new ArrayList<Integer>();
+    public static int longest(int arr[], int target){
 
+        //optimal approach
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum=0;
         int maxlen=0;
-        for(int i=0; i<arr.length; i++){
-            int sum=0;
-            for(int j=i; j<arr.length; j++){
-                sum+=arr[j];
-                if(sum==target){
-                    maxlen = Math.max(maxlen, j-i+1);
-                }
+        for(int i=0; i<arr.length;i++){
+            sum+=arr[i];
+            if(sum==target) maxlen = i + 1;
+            if(!map.containsKey(sum)) map.put(sum, i);
+            if(map.containsKey(sum-target)){
+                int len = i - map.get(sum-target);
+                maxlen = Math.max(maxlen, len);
             }
-            
         }
+        System.out.println(map);
         return maxlen;
 
+
+        // brute force n^2 
+        // int maxlen=0;
+        // for(int i=0; i<arr.length; i++){
+        //     int sum=0;
+        //     for(int j=i; j<arr.length; j++){
+        //         sum += arr[j];
+        //         if(sum==target){
+        //             maxlen = Math.max(maxlen, j-i+1);
+        //         }
+        //     }
+        // }
+        // return maxlen;
     }
 }
