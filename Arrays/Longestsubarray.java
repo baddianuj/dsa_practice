d985308e4533f4602e5423c7b9f2c2d5ef893bff
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-12 16:34:00 +0530

    2pointer longest subarray

--- Arrays/Longestsubarray.java
+++ Arrays/Longestsubarray.java
@@ -8,22 +8,45 @@ public class Longestsubarray {
     }
     public static int longest(int arr[], int target){
 
-        //optimal approach
 
-        Map<Integer, Integer> map = new HashMap<>();
-        int sum=0;
-        int maxlen=0;
-        for(int i=0; i<arr.length;i++){
-            sum+=arr[i];
-            if(sum==target) maxlen = i + 1;
-            if(!map.containsKey(sum)) map.put(sum, i);
-            if(map.containsKey(sum-target)){
-                int len = i - map.get(sum-target);
-                maxlen = Math.max(maxlen, len);
+
+        //optimal apraoch using 2 pointers; handling -ves will be problem , stick to hashmap approach
+        
+        int sum = arr[0];
+        int i=0,j=0;
+        int len=0;
+        while(j<arr.length){
+            // sum+=arr[j];
+           
+            while(sum>target){
+                 sum-=arr[i];
+                 i++;
             }
+            if(sum==target) len = Math.max(len, j-i+1);
+            j++;
+            if(j<arr.length) sum +=arr[j];
+            // len = Math.max(len, j-i+1);
         }
-        System.out.println(map);
-        return maxlen;
+         return len;
+
+
+
+        //better  approach
+
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int sum=0;
+        // int maxlen=0;
+        // for(int i=0; i<arr.length;i++){
+        //     sum+=arr[i];
+        //     if(sum==target) maxlen = i + 1;
+        //     if(!map.containsKey(sum)) map.put(sum, i);
+        //     if(map.containsKey(sum-target)){
+        //         int len = i - map.get(sum-target);
+        //         maxlen = Math.max(maxlen, len);
+        //     }
+        // }
+        // System.out.println(map);
+        // return maxlen;
 
 
         // brute force n^2 
