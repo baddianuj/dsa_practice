270afc65aedc29b440eaae8e7fe4074aedbbb0a8
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-15 19:13:04 +0530

    merge intervals, duplicates optimal soln

--- SDE/Arrays/DupliNum.java
+++ SDE/Arrays/DupliNum.java
@@ -5,12 +5,29 @@ import java.util.*;
 public class DupliNum {
 
     public static int findDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<Integer>();
-        for(int i=0; i<nums.length; i++){
-            if(set.contains(nums[i]))return nums[i];
-            set.add(nums[i]);
+
+
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow!=fast);
+        fast = nums[0];
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return slow;
+
+
+
+        // Set<Integer> set = new HashSet<Integer>();
+        // for(int i=0; i<nums.length; i++){
+        //     if(set.contains(nums[i]))return nums[i];
+        //     set.add(nums[i]);
+        // }
+        // return -1;
     }
 
 
--- SDE/Arrays/MergeIntervals.java
+++ SDE/Arrays/MergeIntervals.java
@@ -8,36 +8,36 @@ public class MergeIntervals {
             {8,10},
             {15,18}
         };
-        System.out.println(Arrays.deepToString(merge(intervals)));
+        int temp[][] = intervals;
+        System.out.println(Arrays.deepToString(merge(temp)));
     }
 
     public static int[][] merge(int[][] intervals) {
-
 
         //chat gpt solution 
 
 
 
-        // if (intervals.length <= 1) return intervals;
+        if (intervals.length <= 1) return intervals;
 
-        // // Step 1: Sort by start time
-        // Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        // Step 1: Sort by start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        // List<int[]> merged = new ArrayList<>();
-        // int[] current = intervals[0];
+        List<int[]> merged = new ArrayList<>();
+        int[] current = intervals[0];
 
-        // for (int i = 1; i < intervals.length; i++) {
-        //     // If overlapping
-        //     if (intervals[i][0] <= current[1]) {
-        //         current[1] = Math.max(current[1], intervals[i][1]); // merge
-        //     } else {
-        //         merged.add(current); // push merged interval
-        //         current = intervals[i]; // move to next
-        //     }
-        // }
+        for (int i = 1; i < intervals.length; i++) {
+            // If overlapping
+            if (intervals[i][0] <= current[1]) {
+                current[1] = Math.max(current[1], intervals[i][1]); // merge
+            } else {
+                merged.add(current); // push merged interval
+                current = intervals[i]; // move to next
+            }
+        }
 
-        // merged.add(current); // add last one
+        merged.add(current); // add last one
 
-        // return merged.toArray(new int[merged.size()][]);
+        return merged.toArray(new int[merged.size()][]);
     }
 }
