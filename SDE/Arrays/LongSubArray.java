7be59143d8f4465a0c2dd1373d840ca7a5957bd5
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-22 16:39:32 +0530

    wefwefwef

--- SDE/Arrays/LongSubArray.java
+++ SDE/Arrays/LongSubArray.java
@@ -1,11 +1,23 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class LongSubArray {
+class LongSubArray {
     public static void main(String[] args) {
         int arr[] = {9, -3, 3, -1, 6, -5};
-        longestSubarray0(arr);
+        int ans = longestSubarray0(arr);
+        System.out.println(ans);
     }
     public static int longestSubarray0(int nums[]){
-       
+    //    ArrayList<Integer> list = new ArrayList<Integer>();
+        int count =0;
+       for(int i=0; i<nums.length; i++){
+        int sum=0;
+        for(int j=i+1; j<nums.length; j++){
+            sum+=nums[j];
+            if(sum==0){
+                count++;
+            }
+        }
+       }
+       return count;
     }
 }
