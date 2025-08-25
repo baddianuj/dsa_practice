51a87ac3ab62a68c0e12bbd0cd4e3a64ce72bdcc
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-08-25 11:43:25 +0530

    linked list - create, search, length

--- SDE/LinkedLists/LL.java
+++ SDE/LinkedLists/LL.java
@@ -27,10 +27,39 @@ public class LL{
         return head;
     }
 
+    private static int length (Node head){
+        Node temp = head;
+        int count = 0;
+        while (temp!=null) {
+            temp = temp.next;
+            count++;
+        }
+        return count;
+    }
+
+    private static boolean search(Node head, int val){
+        Node temp = head;
+        while (temp!=null) {
+            if (temp.data == val) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6};
         Node head = arr2ll(arr);
-        System.out.println(head.data);
-        // System.out.println(arr2ll(arr));
+        // System.out.println(head.data);
+        Node temp = head;
+        while (temp!=null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+        System.out.println(length(head));
+        System.out.println(search(head, 5));
+
     }
 }
