7394bd7f7b2545e47f43db69bc08fda19dca36e7
baddianuj <baddianuj@gmail.com>
2025-09-07 16:48:16 +0530

    product of digits

--- SDE/recursion/sumofdigits.java
+++ SDE/recursion/sumofdigits.java
@@ -6,8 +6,8 @@ public class sumofdigits {
     }
 
     static int sum(int num){
-        if(num==0) return 1;
-        return num%10 * sum(num/10);
+        if(num==0) return 0;
+        return num%10 + sum(num/10);
     }
     
 }
