014921270c6150ec01ad010ed0f0bfe3779c206a
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-09-15 23:23:41 +0530

    something is wrong here with the recursion logic

---
+++ SDE/recursion/rotatedbs.java
@@ -0,0 +1,19 @@
+public class rotatedbs {
+    public static void main(String[] args) {
+         int arr[] = {5,6,7,8,9,0,1,2,3,4};
+
+        System.out.println(recursionsearch(arr, 9, 0, arr.length-1));
+         
+    }
+    public static int recursionsearch(int arr[], int target, int start, int end){
+        if(start > end ) return -1;
+        int mid = start + (end-start)/2;
+
+        if(arr[mid]==target) return mid;
+        if(arr[start]<=arr[mid]){
+            return recursionsearch(arr, target, start, mid - 1);
+        }
+        return recursionsearch(arr, target, mid + 1, end);
+
+    }
+}
