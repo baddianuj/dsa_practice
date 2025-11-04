20487be03c83d0532985b62bbd102cc061d4750c
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-11-04 16:38:03 +0530

    permutations of strings using recursion

--- SDE/recursion/subset.java
+++ SDE/recursion/subset.java
@@ -2,7 +2,21 @@ import java.util.*;
 
 // permutations, combinations, subsets- non aligned collection
 
+//pattern of taking some elements and removing some elements
 
 public class subset {
+    public static void main(String[] args) {
+        subsets("", "abc");
         
+    }
+
+    public static void subsets(String p, String up){
+        if(up.isEmpty()){ 
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subsets(p+ch, up.substring(1));
+        subsets(p, up.substring(1));
+    }
 }
