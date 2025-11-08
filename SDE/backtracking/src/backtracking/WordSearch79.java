5f371d968ae78c0a630643fbbe43d19c7b55034c
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-11-08 19:56:51 +0530

    failed

--- SDE/backtracking/src/backtracking/WordSearch79.java
+++ SDE/backtracking/src/backtracking/WordSearch79.java
@@ -14,24 +14,37 @@ public class WordSearch79 {
     }
 //needs fixing logic
     public static boolean exist(char[][] board, String word, boolean[][] grid,int r, int c) {
+        if (r<0 || c<0 || r>=board.length || c>=board[0].length) return false;
+
         if(word.isEmpty()){
             return true;
         }
-
         if(!grid[r][c]) return false;
 
         char ch = word.charAt(0);
-        grid[r][c]= false;
-        if(r<board.length-1 && ch==board[r][c]){
-            exist(board,word.substring(1),grid,r+1, c);
-        }if(c<board[0].length-1 && ch==board[r][c]){
-            exist(board,word.substring(1),grid,r, c+1);
-        }if(r>0 && ch==board[r][c]){
-            exist(board,word.substring(1),grid,r-1, c);
-        }if(c>0 && ch==board[r][c]){
-            exist(board,word.substring(1),grid,r, c-1);
+        if(board[r][c]!=ch){
+            if(r>0) return exist(board,word.substring(1),grid,r-1,c);
+            if(c>0) return exist(board,word.substring(1),grid,r,c-1);
+
+            if(r< grid.length-1) return exist(board,word.substring(1),grid,r+1,c);
+            if(c< grid[0].length-1) return exist(board,word.substring(1),grid,r,c+1);
+
+            return false;
         }
+
+        grid[r][c]=false;
+        if(r>0) return exist(board,word.substring(1),grid,r-1,c);
+        if(c>0) return exist(board,word.substring(1),grid,r,c-1);
+
+        if(r< grid.length-1) return exist(board,word.substring(1),grid,r+1,c);
+        if(c< grid[0].length-1) return exist(board,word.substring(1),grid,r,c+1);
         grid[r][c]=true;
+
         return true;
+
+
     }
+
+
+
 }
