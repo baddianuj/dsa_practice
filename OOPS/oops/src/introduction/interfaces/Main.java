31683c13d5a46468ddfbb66923d963e4fdb89306
baddianuj <119026809+baddianuj@users.noreply.github.com>
2025-11-19 13:43:12 +0530

    abstraction, abstract classes and methods, interfaces (multiple inheritace like ex)

--- OOPS/oops/src/introduction/interfaces/Main.java
+++ OOPS/oops/src/introduction/interfaces/Main.java
@@ -2,10 +2,21 @@ package introduction.interfaces;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
+//        Car car = new Car();
+
+//        Engine car = new Car(); // when took the reference as Engine , that inferface doestn hace brake
+        // thats why i need it to cast (Car) to the object
+//        car.start();
+//        car.acc();
+//        ((Car) car).brake(); // needed to cast when i take the Engine reference
+//        car.stop();
+
+
+        NiceCar car = new NiceCar();
+
         car.start();
-        car.acc();
-        car.brake();
-        car.stop();
+        car.startMusic();
+        car.upgradeEngine(new ElectricEngine());
+
     }
 }
---
+++ OOPS/oops/src/introduction/interfaces/NiceCar.java
@@ -0,0 +1,33 @@
+package introduction.interfaces;
+
+public class NiceCar {
+    private Engine engine;
+    private final Media player = new CDPlayer();
+
+    public NiceCar(){
+        engine = new PowerEngine();
+    }
+
+    public NiceCar(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void start(){
+        engine.start();
+    }
+
+    public void stop(){
+        engine.stop();
+    }
+
+    public void startMusic(){
+        player.start();
+    }
+    public void stopMusic(){
+        player.stop();
+    }
+
+    public void upgradeEngine(Engine engine){
+        this.engine = engine;
+    }
+}
