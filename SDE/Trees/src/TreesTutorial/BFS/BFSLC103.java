74f80784eb5e0c4a0a4d088d9ee528b04e278a48
baddianuj <119026809+baddianuj@users.noreply.github.com>
2026-01-12 00:52:22 +0530

    lc  103

--- SDE/Trees/src/TreesTutorial/BFS/BFSLC103.java
+++ SDE/Trees/src/TreesTutorial/BFS/BFSLC103.java
@@ -1,17 +1,45 @@
 package TreesTutorial.BFS;
+import java.util.*;
 
 public class BFSLC103 {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root==null) return ans;
+        queue.offer(root);
+        int level = 1;
+        while(!queue.isEmpty()){
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for(int i=0; i<size; i++){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left!=null) queue.offer(node.left);
+                if(node.right!=null) queue.offer(node.right);
+            }
+            if(level%2==0){
+                Collections.reverse(list);
+                ans.add(list);
+            } else {
+                ans.add(list);
+            }
+
+            level++;
+        }
+        return ans;
+    }
 }
-
-da da
-
-public da getDa() {
-
-
-     dada
-
-
-}
-
 
