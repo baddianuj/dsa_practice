f5b815e8bf54df84f70ce694df6d31d63286b5ae
baddianuj <119026809+baddianuj@users.noreply.github.com>
2026-01-12 21:47:15 +0530

    ff

---
+++ SDE/Trees/src/TreesTutorial/BFS/Practice.java
@@ -0,0 +1,17 @@
+package TreesTutorial.BFS;
+
+import java.util.Queue;
+
+public class Practice {
+    public class Node{
+        int val;
+        Node left;
+        Node right;
+        Node next;
+    }
+
+
+    public static void main(String[] args) {
+        Queue
+    }
+}
